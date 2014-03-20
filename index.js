670ef0f06b81cf1e000deac7ca0a93b283cd07cb
whodatninja <whodatninja@outlook.com>
2014-03-20 15:37:43 -0400

    Fix library path

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
+var path = require('path');
 var vm = require('vm');
 
-srpCode = fs.readFileSync('./src/srp-client.js', 'utf8');
+var srpPath = path.join(__dirname, 'src', 'srp-client.js');
+var srpCode = fs.readFileSync(srpPath, 'utf8');
 vm.runInThisContext(srpCode);
 module.exports = SRPClient;
