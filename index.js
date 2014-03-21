a5e20bf354415a6b63cbb1804418966e3720c6e0
whodatninja <whodatninja@outlook.com>
2014-03-21 17:14:51 -0400

    Fix vm context to include require

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var vm = require('vm');
 
 var srpPath = path.join(__dirname, 'src', 'srp-client.js');
 var srpCode = fs.readFileSync(srpPath, 'utf8');
-vm.runInThisContext(srpCode);
-module.exports = SRPClient;
+
+var srp = vm.runInNewContext(srpCode, {require: require});
+module.exports = srp.SRPClient;
