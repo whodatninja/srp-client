b3ac80439c8c45817fe27f07ebf75d2dc114390c
whodatninja <whodatninja@outlook.com>
2014-03-21 17:57:43 -0400

    Fix module.exports for real this time

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var fs = require('fs');
 var path = require('path');
 var vm = require('vm');
 
+
 var srpPath = path.join(__dirname, 'src', 'srp-client.js');
 var srpCode = fs.readFileSync(srpPath, 'utf8');
+var sandbox = {require: require};
+vm.runInNewContext(srpCode, sandbox);
 
-module.exports = vm.runInNewContext(srpCode, {require: require});
+module.exports = sandbox.SRPClient;
